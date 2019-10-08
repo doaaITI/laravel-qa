f674f13665736c5102ed20b00dfab6530554b658
doaa eldahab <you@example.com>
2019-10-08 09:55:53 +0200

    lesson 7

--- app/Question.php
+++ app/Question.php
@@ -17,4 +17,15 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
